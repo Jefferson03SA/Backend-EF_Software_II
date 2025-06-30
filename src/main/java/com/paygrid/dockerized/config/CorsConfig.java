edc001233619d0550ed94d77a4b9a67f9c7c731f
jbrays <bryan20santillan@gmail.com>
2025-06-30 00:35:18 -0500

    cors allow 4200 port

--- src/main/java/com/paygrid/dockerized/config/CorsConfig.java
+++ src/main/java/com/paygrid/dockerized/config/CorsConfig.java
@@ -1,20 +1,31 @@
 package com.paygrid.dockerized.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
+
+    @Value("${CORS_ALLOWED_ORIGINS}")
+    private String corsAllowedOrigins;
 
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200"); // Permitir origen del frontend
+
+        // Leer los orígenes permitidos de la variable de entorno
+        List<String> origins = Arrays.asList(corsAllowedOrigins.split(","));
+        config.setAllowedOrigins(origins);
+
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
